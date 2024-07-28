6135933692ecae20be34f8c060e69af25fbe3d4b
MilciadesFerreira05 <milciadesferreirs5@gmail.com>
2024-07-28 05:32:11 -0400

    creacion de vista de productsList

--- frontend_pdv/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Asegúrate de que esta URL apunte a tu API de Spring Boot
-});
-
-export default api;
